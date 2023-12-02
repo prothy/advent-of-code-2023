3662b0f60e7d973f8aa9435d5b75787ed8b3fdbf
Luka Csurgay <luka.csurgay@accedo.tv>
2023-12-02 13:53:03 +0100

    read file async

--- utils.js
+++ utils.js
@@ -1,11 +1,13 @@
 import { open } from 'fs/promises';
 import * as readline from 'readline/promises';
 
-export async function processLinesInFile(callback) {
+export async function processLinesInFile(handler) {
   const file = await open('input.txt');
   const rl = readline.createInterface({ input: file.createReadStream() });
 
-  for await (const line of rl) {
-    callback(line);
-  }
+  rl.on('line', handler);
+
+  return new Promise((resolve) => {
+    rl.on('close', resolve);
+  });
 }
